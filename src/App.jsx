a739b84090d7e0cd17be39ce1cd0f6e2c5932e50
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-12-01 16:08:57 +0530

    Setup: project and packages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl text-amber-900">
+        Hi there, Initialising the project
+      </h1>
+    </>
+  );
+}
+
+export default App;
